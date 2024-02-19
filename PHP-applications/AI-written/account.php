078affba784936a2b009d8423f69f6439f531b1a
MohammadReza8480 <mmahinfar630@gmail.com>
2024-02-19 13:15:07 +0330

    Add delete account option to account.php

--- PHP-applications/AI-written/account.php
+++ PHP-applications/AI-written/account.php
@@ -8,20 +8,34 @@ if (!isset($_SESSION['username'])) {
     exit();
 }
 
+$username = $_SESSION['username'];
+if (isset($_POST['delete_account'])) {
+    $sql = "DELETE FROM users WHERE username='$username'";
+    mysqli_query($conn, $sql);
+    // Destroy the session
+    session_destroy();
+    // Optionally, delete the cookie:
+    setcookie('remember_token', '', time() - 3600, '/');
+    header('Location: index.php');
+}
+
 ?>
 
 <head>
 <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
 </head>
 <style>
-        body{ font: 14px sans-serif; width: 800px; padding: 25px; margin: 0 auto; padding-top: 100px; background-color: #262626; color:#FFFF; }
+        body{ font: 14px sans-serif; width: 1000px; padding: 25px; margin: 0 auto; padding-top: 100px; background-color: #262626; color:#FFFF; }
 </style>
 <body>
-    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($_SESSION["username"]); ?></b>. Welcome to our site.</h1>
+    <h1 class="my-5">Hi, <b><?php echo htmlspecialchars($username); ?></b>. Welcome to our site.</h1>
     <div>
-        <a href="profile.php" class="btn btn-info ml-3">View Profile</a>
-        <a href="change_password.php" class="btn btn-warning ml-3">Change Your Password</a>
-        <a href="logout.php" class="btn btn-danger ml-3">Sign Out of Your Account</a>
+        <form action="account.php" method="POST">
+            <a href="profile.php" class="btn btn-info ml-3">View Profile</a>
+            <a href="change_password.php" class="btn btn-primary ml-3">Change Your Password</a>
+            <a href="logout.php" class="btn btn-warning ml-3">Sign Out of Your Account</a>
+            <button type="submit" name="delete_account" class="btn btn-danger ml-3">Delete Account</button>
+        </form>
     <div>
 </body>
 </html>
